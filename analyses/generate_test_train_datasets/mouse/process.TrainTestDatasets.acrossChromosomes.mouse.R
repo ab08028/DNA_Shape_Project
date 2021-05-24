2589f828276f6871fb02cdd13e8e4e8d28cc14db
Annabel Beichman <annabel.beichman@gmail.com>
2021-05-24 12:34:55 -0700

    test

--- analyses/generate_test_train_datasets/mouse/process.TrainTestDatasets.acrossChromosomes.mouse.R
+++ analyses/generate_test_train_datasets/mouse/process.TrainTestDatasets.acrossChromosomes.mouse.R
@@ -44,6 +44,29 @@ for(state in c("TESTING.even_bpOnly","TRAINING.odd_bpOnly")){
     group_by(population,state) %>%
     mutate(fractionOfAllSegSites=totalSitesAllIntervals/sum(totalSitesAllIntervals))  
   
+  
+  ####### add in metadata about mutation types:
+  allIntervalSpectra_totals_SummedUp$ancestralkmer <- unlist(lapply(strsplit(as.character(allIntervalSpectra_totals_SummedUp$variable),"\\."),"[",1))
+  allIntervalSpectra_totals_SummedUp$derivedkmer <- unlist(lapply(strsplit(as.character(allIntervalSpectra_totals_SummedUp$variable),"\\."),"[",2))
+
+  # "AAAAAAA.AAACAAA" so the position of the central mutations are 4 and 12 (remember ther's a dot in between')
+  # and 3-5 is teh 3mer context and 11-13
+  allIntervalSpectra_totals_SummedUp$centralMutationType <- paste(substr(allIntervalSpectra_totals_SummedUp$variable,4,4),substr(allIntervalSpectra_totals_SummedUp$variable,12,12),sep=".")
+  allIntervalSpectra_totals_SummedUp$ThreemerMutationType <- paste(substr(allIntervalSpectra_totals_SummedUp$variable,3,5),substr(allIntervalSpectra_totals_SummedUp$variable,11,13),sep=".")
+  
+  # classify mutation types:
+  # identify presence of CpG (CG) [not GC] in central 3mer
+  allIntervalSpectra_totals_SummedUp$ancestral_central3mer <- unlist(lapply(strsplit(allIntervalSpectra_totals_SummedUp$ThreemerMutationType,"\\."),"[",1))
+  allIntervalSpectra_totals_SummedUp$derived_central3mer <- unlist(lapply(strsplit(allIntervalSpectra_totals_SummedUp$ThreemerMutationType,"\\."),"[",2))
+  
+  allIntervalSpectra_totals_SummedUp$ancestral_central3merCpG <- "no"
+  allIntervalSpectra_totals_SummedUp$derived_central3merCpG <- "no"
+  allIntervalSpectra_totals_SummedUp[grepl("CG",allIntervalSpectra_totals_SummedUp$ancestral_central3mer),]$ancestral_central3merCpG <- "yes"
+  allIntervalSpectra_totals_SummedUp[grepl("CG",allIntervalSpectra_totals_SummedUp$derived_central3mer),]$derived_central3merCpG <- "yes"
+  
+  allIntervalSpectra_totals_SummedUp$mutationClassLabel <- paste0(allIntervalSpectra_totals_SummedUp$centralMutationType,".",allIntervalSpectra_totals_SummedUp$ancestral_central3merCpG,"_ancestralCpG") 
+ # unique(allIntervalSpectra_totals_SummedUp$mutationClassLabel)
+  
   write.table(allIntervalSpectra_totals_SummedUp,paste0(outdir,species,".",state,".allPops.summedOverChr.spectra.NOSTRICT.txt"),row.names = F,quote=F,sep="\t")
   
 }
