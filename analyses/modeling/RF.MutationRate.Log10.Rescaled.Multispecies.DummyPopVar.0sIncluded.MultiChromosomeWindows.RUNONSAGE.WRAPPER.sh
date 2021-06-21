f18e955d068d842fdf06bcb12f0de2b9733a7191
Annabel Beichman <beichman@grid-head2.grid.gs.washington.edu>
2021-06-21 14:12:37 -0700

    need hdata to be mfree

--- analyses/modeling/RF.MutationRate.Log10.Rescaled.Multispecies.DummyPopVar.0sIncluded.MultiChromosomeWindows.RUNONSAGE.WRAPPER.sh
+++ analyses/modeling/RF.MutationRate.Log10.Rescaled.Multispecies.DummyPopVar.0sIncluded.MultiChromosomeWindows.RUNONSAGE.WRAPPER.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#$ -l h_rt=20:00:00,h_data=20G
+#$ -l h_rt=20:00:00,mfree=25G
 #$ -o /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/modeling
 #$ -e /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/modeling
 #$ -m bea
