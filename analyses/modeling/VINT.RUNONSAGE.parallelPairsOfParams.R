b5b59be5c1e3777cc2a560643c2ff8275a8b1a77
Annabel Beichman <annabel.beichman@gmail.com>
2021-07-01 15:44:13 -0700

    trying to parallelize vint()

--- analyses/modeling/VINT.RUNONSAGE.parallelPairsOfParams.R
+++ analyses/modeling/VINT.RUNONSAGE.parallelPairsOfParams.R
@@ -18,6 +18,8 @@ library(doParallel)
 # for doing this on hoffman you could set 
 args <- commandArgs(trailingOnly = TRUE)
 indir <- paste0(args[1],"/") # doing this to make sure slash comes after
+topXParamsToCompare <- args[2] # number of top params to compare (will affect size of array) == start with 5.
+parameter_pair_index <- args[3] # give the index when you submit the script so you can do parameter pairs in parallel!  
 
 oneFoldSetToTrainAndAssessOn <- readRDS(paste0(indir,"oneFoldSetToTrainAndAssessOn.rds"))
 model <- readRDS(paste0(indir,"modelTrainedOnOneFold.rds"))
@@ -40,7 +42,7 @@ vi_scores <- vip::vi(ranger_obj)
 # just doing top 5 scores *for now* for interactions
 top_vi_scores <- 
   vi_scores %>%
-  filter(rank(desc(Importance))<=5)
+  filter(rank(desc(Importance))<=topXParamsToCompare)
 
 #vip_plot <- vip(vi_scores,include_type = T,num_features = 100)
 #vip_plot
@@ -54,12 +56,13 @@ top_vi_scores <-
 
 # get all pairs of top scores:
 pairsOfTopScores <- combn(top_vi_scores$Variable,m=2,simplify = T) # get all pairs of top scores  == I checked that combn will return things in the same order, so I can use SGE_TASK_ID to pull different rows of combn out in parallel 
-allInteractionResults <- NULL
-for (i in seq(1,dim(pairsOfTopScores)[2])){
+#allInteractionResults <- NULL
+#for (i in seq(1,dim(pairsOfTopScores)[2])){
   #print(pairsOfTopScores[,i])
-  interact <- vint(ranger_obj,feature_names=pairsOfTopScores[,i],train=analysisdf_processed,progress = "text",parallel=T) 
-  saveRDS(interact,paste0(indir,"vint.intermediate.",i,".rds"))
-  allInteractionResults <- rbind(allInteractionResults,interact)
-  }
-saveRDS(allInteractionResults,paste0(outdir,"vint.rds"))
+interact <- vint(ranger_obj,feature_names=pairsOfTopScores[,parameter_pair_index],train=analysisdf_processed,progress = "text",parallel=T) 
 
+write.table(data.frame(interact),paste0(indir,"vint.parallel.",parameter_pair_index,".txt"),rownames=F,quote=F,sep="\t")
+#allInteractionResults <- rbind(allInteractionResults,interact)
+#  }
+#saveRDS(allInteractionResults,paste0(outdir,"vint.rds"))
+#This function quantifies the strength of interaction between features $X_1$ and $X_2$ by measuring the change in variance along slices of the partial dependence of $X_1$ and $X_2$ on the target $Y$. See Greenwell et al. (2018) for details and examples.
---
+++ analyses/modeling/model_002_RF.MutationRate.Log10.Rescaled.Multispecies.DummyPopVar.VINT.PARALLEL.RUNONSAGE.WRAPPER.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+#$ -l h_rt 40:00:00,mfree=2G
+#$ -o /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/modeling
+#$ -e /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/modeling
+#$ -m bea
+#$ -M annabel.beichman@gmail.com
+#$ -N model_001_vint_parallel
+#$ -pe serial 5
+#$ -t 1-10 
+#### NEED TO CHANGE THIS BASED ON HOW MANY TOP PARAMETERS YOU'RE USING
+# THIS ISN'T WELL AUTOMATED --- JUST GET IT WORKING WELL ENOUGH FOR NOW.
+# NEED TO MANUALLY CALCULATE 5 CHOOSE 2 = 10 TO GET THE SIZE OF THE ARRAY
+######## trying to run modeling script in hoffman #######
+
+###### SAVE NEW WRAPPER SCRIPT EACH TIME ######
+module load modules modules-init modules-gs # initialize modules 
+module load pcre2/10.35 R/4.0.4
+# need to load newer gcc for installations! 
+module load gcc/10.2.0
+# need to set up R environment: 
+#R 
+#install.packages("tidymodels")
+# install.packages("tidyverse")
+# install.packages("workflows")
+# install.packages("tune")
+# install.packages("vip")
+# install.packages("ranger")
+# install.packages("ggplot2")
+# install.packages("ggrepel")
+# install.packages("ggbeeswarm")
+# install.packages("reshape2")
+# install.packages("devtools")
+
+# need a data dir with shapes
+# need a data dir with species data 
+# these are just inside the R script
+#spectrumdir=/net/harris/vol1/home/beichman/DNAShape/spectrumDataForModeling/mouse
+#shapedir=/net/harris/vol1/home/beichman/DNAShape/shapeDataForModeling
+
+gitdir=/net/harris/vol1/home/beichman/scriptsAndGitDirs/DNA_Shape_Project/analyses/modeling
+scriptdir=$gitdir/
+
+# but date this running script each time
+
+todaysdate=`date +%Y%m%d`
+##### CHANGE THESE APPROPRIATELY TO BE ABOUT SCRIPT YOU"RE RUNNING #########
+#outcomeLabel="FracSegSites.Log10"
+#modelLabel="RF"
+#otherInfo="Multispecies.DummyPopVar"
+
+#description=${modelLabel}.${outcomeLabel}.${otherInfo}
+
+
+description=model_001_RF.MutationRate.Log10.Multispecies.DummyPopVar
+indir="/net/harris/vol1/home/beichman/DNAShape/analyses/modeling/experiments/20210618"_${description}"/" # must end in "/"
+#script=${description}.RUNONSAGE.R # this may not be the best way to do this
+#script=RF.FracSegSites.Log10.Multispecies.DummyPopVar.2.VINT.RUNONSAGE.R
+script=VINT.RUNONSAGE.parallelPairsOfParams.R
+
+# need to specify top number of parameters (5) to compare interactions of and also the index will be SGE_TASK_ID
+
+
+topXParams=5
+Rscript $scriptdir/$script $indir $topXParams $SGE_TASK_ID
+
+# would like output files and the script to be copied to the same outdir as a record. 
+
