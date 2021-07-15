822ec52cfabed4dfc74297b142475697b6568567
Annabel Beichman <annabel.beichman@gmail.com>
2021-07-15 08:42:05 -0700

    updating runtime of shap

--- analyses/modeling/model_009_SHAPValueCalculations.WRAPPER.sh
+++ analyses/modeling/model_009_SHAPValueCalculations.WRAPPER.sh
@@ -1,4 +1,4 @@
-#$ -l h_rt=20:00:00,mfree=25G
+#$ -l h_rt=100:00:00,mfree=25G
 #$ -o /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/modeling
 #$ -e /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/modeling
 #$ -m bea
