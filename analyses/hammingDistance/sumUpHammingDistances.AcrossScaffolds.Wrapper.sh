df4343396b4c0998360cde0e4d0cf44e1d91b163
Annabel Beichman <beichman@grid-head1.grid.gs.washington.edu>
2021-11-09 15:23:48 -0800

    fixing path

--- analyses/hammingDistance/sumUpHammingDistances.AcrossScaffolds.Wrapper.sh
+++ analyses/hammingDistance/sumUpHammingDistances.AcrossScaffolds.Wrapper.sh
@@ -12,7 +12,8 @@ module load pcre2/10.35 hdf5/1.10.1 R/4.0.4
 module load gcc/10.2.0 # necessary for reshape2
 
 script=sumUpHammingDistances.AcrossScaffolds.R
-scriptdir=/Users/annabelbeichman/Documents/UW/DNAShapeProject/scripts/DNA_Shape_Project/analyses/hammingDistance
+scriptdir=/net/harris/vol1/home/beichman/scriptsAndGitDirs/DNA_Shape_Project/analyses/hammingDistance
+
 
 Rscript $scriptdir/$script
 
