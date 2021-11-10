3d7261d11110d8d3d59cbf591a5cd40b9c8a657f
Annabel Beichman <beichman@grid-head1.grid.gs.washington.edu>
2021-11-09 16:45:46 -0800

    runs much faster with higher ram request (must be mfree not hdata)

--- analyses/hammingDistance/sumUpHammingDistances.AcrossScaffolds.Wrapper.sh
+++ analyses/hammingDistance/sumUpHammingDistances.AcrossScaffolds.Wrapper.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#$ -l h_rt=10:00:00,h_data=5G
+#$ -l h_rt=10:00:00,mfree=30G
 #$ -o /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/hamming
 #$ -e /net/harris/vol1/home/beichman/DNAShape/reports.nobackup/hamming
 #$ -m bea
