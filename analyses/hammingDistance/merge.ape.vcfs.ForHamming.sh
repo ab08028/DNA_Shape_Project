d082eeba9d376bf838dd96be1edfcef4a51e1974
Annabel Beichman <annabel.beichman@gmail.com>
2021-12-06 11:33:43 -0800

    callability masked merged ape vcf

--- analyses/hammingDistance/merge.ape.vcfs.ForHamming.sh
+++ analyses/hammingDistance/merge.ape.vcfs.ForHamming.sh
@@ -18,8 +18,12 @@ outdir=/net/harris/vol1/home/beichman/apes/merged_vcf
 
 # merge them with bcftools
 # all lets all things be merged so no dup records are created (hopefully)
-# use the bcf of PanTro
-bcftools merge -m all $vcfdir/Gorilla.vcf.gz $vcfdir/Pan_paniscus.vcf.gz $vcfdir/Pan_troglodytes.bcf $vcfdir/Pongo_abelii.vcf.gz $vcfdir/Pongo_pygmaeus.vcf.gz -Ou | bcftools view -c 1:minor -m2 -M2 -v snps -Ou | bcftools convert -Oz -o $outdir/ALLAPES.mergedVCFs.ForHammingDistance.vcf.gz
+# use the bcf of PanTro 
+
+whole_callability_mask=/net/harris/vol1/home/beichman/apes/callability_mask/Intersect_filtered_cov8.bed.gz # same for all species 
+
+
+bcftools merge -m all $vcfdir/Gorilla.vcf.gz $vcfdir/Pan_paniscus.vcf.gz $vcfdir/Pan_troglodytes.bcf $vcfdir/Pongo_abelii.vcf.gz $vcfdir/Pongo_pygmaeus.vcf.gz -Ou | bcftools view -c 1:minor -m2 -M2 -v snps -R $whole_callability_mask -Ou | bcftools convert -Oz -o $outdir/ALLAPES.mergedVCFs.ForHammingDistance.CALLABILITYMASKED.vcf.gz
 # then restrict to just snps again 
 
 # note this is going to be a bit different from other species 
