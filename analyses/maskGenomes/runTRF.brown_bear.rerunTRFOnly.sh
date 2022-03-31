f56c1a861c2de905606603cde497fd0ab89da959
Annabel Beichman <annabel.beichman@gmail.com>
2022-03-31 12:35:37 -0700

    adding i=

--- analyses/maskGenomes/runTRF.brown_bear.rerunTRFOnly.sh
+++ analyses/maskGenomes/runTRF.brown_bear.rerunTRFOnly.sh
@@ -54,7 +54,7 @@ genomeFasta=brown_bear.fasta
 #4. Minscore: The alignment of a tandem repeat must meet or exceed this alignment score to be reported. For example, if we set the matching weight to 2 and the minimun score to 50, assuming perfect alignment, we will need to align at least 25 characters to meet the minimum score (for example 5 copies with a period of size 5). [not sure if should keep as is ?] 
 #5. Maxperiod: Period size is the program's best guess at the pattern size of the tandem repeat. The program will find all repeats with period size between 1 and 2000, but the output can be limited to a smaller range. [[[ In UCSC this is set to 12 ]]] 
 
-mkdir $wd/trf
+mkdir -p $wd/trf
 cd $wd/trf # maybe this will work
 trf -h -d 2 5 7 80 10 50 12 $wd/$genomeFasta
 # h: suppresss html
